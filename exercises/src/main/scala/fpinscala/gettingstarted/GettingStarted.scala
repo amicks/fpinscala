c7aa4d7a5784513b031fba2383197f2fd2a38511
Allston Mickey <allston.mickey@gmail.com>
2019-02-09 21:57:33 -0800

    Add 2_3

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -167,7 +167,7 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    a => b => f(a, b) // Scala knows the types of these arguments b/c they are implied by the return type
 
   // NB: The `Function2` trait has a `curried` method already
 
